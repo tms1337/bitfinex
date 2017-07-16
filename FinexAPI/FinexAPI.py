bd4905ee63d8f8813fbcccc9d9ee5390ff23a1b7
tms1337 <faruk1337@gmail.com>
2017-07-16 17:25:02 +0200

    ignore file opening error

--- FinexAPI/FinexAPI.py
+++ FinexAPI/FinexAPI.py
@@ -13,7 +13,10 @@ __all__ = ['fp', 'ticker', 'today', 'orderbook', 'lendbook', 'stats', 'trades', 
 
 URL = "https://api.bitfinex.com/v1"
 
-fp = open("../keys.txt")
+try:
+    fp = open("../keys.txt")
+except Exception:
+    pass
 
 API_KEY = fp.readline().rstrip() # put your API public key here.
 API_SECRET = fp.readline().rstrip() # put your API private key here.
